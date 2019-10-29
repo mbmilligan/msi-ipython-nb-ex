d7abfd1ef04f996a2adb6a9fc7059a750564d375
Michael Milligan <milligan@msi.umn.edu>
2019-10-29 12:54:57 -0500

    update mpitest for Python 3

--- notebook/AdvPyFiles/mpitest.py
+++ notebook/AdvPyFiles/mpitest.py
@@ -14,19 +14,19 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 if rank == 0:
-    data = [(i+1)**2 for i in xrange(size)]
+    data = [(i+1)**2 for i in range(size)]
 else:
    data = None
 
-print 'Rank, Data as initialized: ', rank, data
+print( 'Rank, Data as initialized: ', rank, data)
 stdout.flush()
 
 data = comm.scatter(data, root=0)
-print 'Rank, Data after scatter: ', rank, data
+print('Rank, Data after scatter: ', rank, data)
 stdout.flush()
 
 data += 100
 data = comm.gather(data, root=0)
 
-print 'Rank, Data after gather: ', rank, data
+print('Rank, Data after gather: ', rank, data)
 stdout.flush()
